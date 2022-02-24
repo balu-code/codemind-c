0e02d7cd2b2ea84e6400f93272f5b76778357d98
balu-code <98811930+balu-code@users.noreply.github.com>
2022-02-24 12:52:10 +0530

    #include<stdio.h> int main() {     int L,B,W,C,area,Tot_cost;     scanf("%d%d%d%d",&L,&B,&W,&C);     if(L<=2*W||B<=2*W)     printf("Impossible");     else     {     area=((L*B)-(L-2*W)*(B-2*W));     Tot_cost=area*C;     printf("%d",Tot_cost);
        }
    return 0;
    }

---
+++ Cost_to_fence_the_park_inside.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int L,B,W,C,area,Tot_cost;
+    scanf("%d%d%d%d",&L,&B,&W,&C);
+    if(L<=2*W||B<=2*W)
+    printf("Impossible");
+    else
+    {
+    area=((L*B)-(L-2*W)*(B-2*W));
+    Tot_cost=area*C;
+    printf("%d",Tot_cost);
+   
+        
+    }
+return 0;
+}
+   
+        
