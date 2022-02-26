c4cdac2fc5fd812b0bd5ebd8cba263335108a4dc
balu-code <98811930+balu-code@users.noreply.github.com>
2022-02-26 16:18:25 +0530

    #include<stdio.h> main() {  int n,x[20],odd=0,i;  scanf("%d",&n);  for(i=0;i<n;i++)  scanf("%d",&x[i]);  for(i=0;i<n;i++)  { if(x[i]%2!=0)      odd++;  }  if(odd<=2)  printf("YES");  else  printf("NO"); }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+ int n,x[20],odd=0,i;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ scanf("%d",&x[i]);
+ for(i=0;i<n;i++)
+ { if(x[i]%2!=0)
+     odd++;
+ }
+ if(odd<=2)
+ printf("YES");
+ else
+ printf("NO");
+}
