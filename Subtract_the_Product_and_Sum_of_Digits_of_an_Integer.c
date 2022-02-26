12edc5c03297625c9dd1de46fae6a43f75a129a6
balu-code <98811930+balu-code@users.noreply.github.com>
2022-02-26 19:57:43 +0530

    #include<stdio.h> main() {     int n,sum=0,rem,product=1;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         sum=sum+rem;         product=product*rem;         n=n/10;     }     printf("%d", product-sum); }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+    int n,sum=0,rem,product=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        product=product*rem;
+        n=n/10;
+    }
+    printf("%d", product-sum);
+}
+        
+    
+    
